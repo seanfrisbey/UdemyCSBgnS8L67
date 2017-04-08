9352bb42860c13e771a9181b7d34af4057ece3e1
Sean Frisbey <seanfrisbey@gmail.com>
2017-04-07 22:00:15 -0700

    Initial commit

---
+++ P5/Program.cs
@@ -0,0 +1,29 @@
+﻿/***********************************************************
+ * Write a program and ask the user to enter an English
+ * word. Count the number of vowels (a, e, o, u, i) in the
+ * word. So, if the user enters "inadequate", the program
+ * should display 6 on the console.
+ **********************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an English word: ");
+            var inputWord = Console.ReadLine().ToLower();
+            
+            for (var i = 0; i < inputWord.Length; i++)
+            {
+                if 
+            }
+        }
+    }
+}
